2d85ccc10ad3a729c6146e22e79990e82a9c97d8
Kardoost <kardoost@win.desy.de>
2022-01-17 23:07:34 +0100

    load all the manual data to variables and check the accepted values

--- app.js
+++ app.js
@@ -37,9 +37,121 @@ var overviewimage_itemNamesData = {"OverviewImageValidity":"", "MicroscopeData":
 var roi_itemNamesData = {"ROIImageValidity":"", "MicroscopeData":"", "DetectionMethod":"", 
     "ROIOverviewImageLoad":"", "ROIExperimentDate":"", "ROIExperimentTime":""};
 
+var sample_itemNamesData = {"SampleImageValidity":"", "PixelSizeX":"", "PixelSizeY":"", "BBXPosition":"", 
+    "BBYPosition":"", "BBWidth":"", "BBHeight":"", "MaskImageLoad":"", "SampleimageExperimentDate":"", 
+    "SampleimageExperimentTime":"", "SampleComments":"", "SampleInfoValidity":"", "SampleMicroscopeData":"", 
+    "SampleDetectionMethod":"", "ROIImageLoad":"", "ImageType":"", "ImageWidth":"", "ImageHeight":"", 
+    "ImageSize":"", "ImageComments":"", "ImageLoad":"", "SampleinfoExperimentDate":"", "SampleinfoExperimentTime":""};
 
+var target_itemNamesData = {"TargetImageValidity":"", "TargetStatus":"", "ThetaAngle":"", "PhiAngle":"", 
+    "RhoAngle":"", "TargetinfoExperimentDate":"", "TargetinfoExperimentTime":"", "TargetComments":"", 
+    "TargetValidity":"", "TargetXPosition":"", "TargetYPosition":"", "TargetZPosition":"", "InplaneAccuracy":"", 
+    "OutOfPlaneAccuracy":"", "TargetpositioninfoExperimentDate":"", "TargetpositioninfoExperimentTime":"", 
+    "InstrumentUsed":"", "XFELDataExperimentDate":"", "XFELDataExperimentTime":"", "TargetInstrumentComments":""};
 
+function checkEmpty(term, page_name) {
+    var result = false;
 
+    if (page_name === "subframe"){
+        value_0 = (term.SubframeValidity===false ? true:false);
+        value_1 = (term.FacilityNameSelect==="Choose..." ? true:false);
+        value_2 = (term.SubframeTypeSelect==="Choose..." ? true:false);
+        value_3 = (term.GroupName==="Choose..." ? true:false);
+        value_4 = (term.SizeX==='' ? true:false);
+        value_5 = (term.SizeY==='' ? true:false);
+        value_6 = (term.SubframeExperimentDate==='' ? true:false);
+        value_7 = (term.SubframeExperimentTime==='' ? true:false);
+        value_8 = (term.SubframeEventTypeName==="Choose..." ? true:false);
+        result = (value_0 | value_1 | value_2 | value_3 | value_4 | value_5 | value_6 | value_7 | value_8);
+    } else if (page_name === "fiducial"){
+        if ((term.Fid1Validity + term.Fid2Validity + term.Fid3Validity + term.Fid4Validity) < 3) {
+            result = true;
+        }else {
+            value_0 = (term.Fid1PositionName === 'Choose...' ? true:false);
+            value_1 = (term.Fid2PositionName === 'Choose...' ? true:false);
+            value_2 = (term.Fid3PositionName === 'Choose...' ? true:false);
+            value_3 = (term.Fid4PositionName === 'Choose...' ? true:false);
+            value_4 = (term.Fid1XPosition === '' ? true:false);
+            value_5 = (term.Fid1YPosition === '' ? true:false);
+            value_6 = (term.Fid1ZPosition === '' ? true:false);
+            value_7 = (term.Fid2XPosition === '' ? true:false);
+            value_8 = (term.Fid2YPosition === '' ? true:false);
+            value_9 = (term.Fid2ZPosition === '' ? true:false);
+            value_10 = (term.Fid3XPosition === '' ? true:false);
+            value_11 = (term.Fid3YPosition === '' ? true:false);
+            value_12 = (term.Fid3ZPosition === '' ? true:false);
+            value_13 = (term.Fid4XPosition === '' ? true:false);
+            value_14 = (term.Fid4YPosition === '' ? true:false);
+            value_15 = (term.Fid4ZPosition === '' ? true:false);
+            result = (value_0 | value_1 | value_2 | value_3 | value_4 | value_5 | value_6 | value_7 | value_8 | 
+                value_9 | value_10 | value_11 | value_12 | value_13 | value_14 | value_15);
+        }
+    }  else if (page_name === "overviewimage") {
+        value_0 = (term.OverviewImageValidity===false ? true:false);
+        value_1 = (term.MicroscopeData==="Choose..." ? true:false);
+        value_2 = (term.DetectionMethod==="Choose..." ? true:false);
+        value_3 = (term.OverviewImageLoad==='' ? true:false);
+        value_4 = (term.OverviewImageExperimentDate==='' ? true:false);
+        value_5 = (term.OverviewImageExperimentTime==='' ? true:false);
+        result = (value_0 | value_1 | value_2 | value_3 | value_4 | value_5);
+    } else if (page_name === "roi") {
+        value_0 = (term.ROIImageValidity===false ? true:false);
+        value_1 = (term.MicroscopeData==="Choose..." ? true:false);
+        value_2 = (term.DetectionMethod==="Choose..." ? true:false);
+        value_3 = (term.ROIOverviewImageLoad==='' ? true:false);
+        value_4 = (term.ROIExperimentDate==='' ? true:false);
+        value_5 = (term.ROIExperimentTime==='' ? true:false);
+        result = (value_0 | value_1 | value_2 | value_3 | value_4 | value_5);
+    } else if (page_name==="sampleimage"){
+        value_0 = (term.SampleImageValidity===false ? true:false);
+        value_1 = (term.PixelSizeX==='' ? true:false);
+        value_2 = (term.PixelSizeY==='' ? true:false);
+        value_3 = (term.BBXPosition==='' ? true:false);
+        value_4 = (term.BBYPosition==='' ? true:false);
+        value_5 = (term.BBWidth==='' ? true:false);
+        value_6 = (term.BBHeight==='' ? true:false);
+        value_7 = (term.MaskImageLoad==='' ? true:false);
+        value_8 = (term.SampleimageExperimentDate==='' ? true:false);
+        value_9 = (term.SampleimageExperimentTime==='' ? true:false);
+        value_10 = (term.SampleInfoValidity===false ? true:false);
+        value_11 = (term.SampleMicroscopeData==="Choose..." ? true:false);
+        value_12 = (term.SampleDetectionMethod==="Choose..." ? true:false);
+        value_13 = (term.ROIImageLoad==='' ? true:false);
+        value_14 = (term.ImageType==="Choose..." ? true:false);
+        value_15 = (term.ImageWidth==='' ? true:false);
+        value_16 = (term.ImageHeight==='' ? true:false);
+        value_17 = (term.ImageSize==='' ? true:false);
+        value_18 = (term.ImageLoad==='' ? true:false);
+        value_19 = (term.SampleinfoExperimentDate==='' ? true:false);
+        value_20 = (term.SampleinfoExperimentTime==='' ? true:false);
+        result = (value_0 | value_1 | value_2 | value_3 | value_4 | value_5 | value_6 | value_7 | value_8 | 
+            value_9 | value_10 | value_11 | value_12 | value_13 | value_14 | value_15 | value_16 | value_17 |
+            value_18 | value_19 | value_20);
+    } else if (page_name==="targetdata") {
+        value_0 = (term.TargetImageValidity===false ? true:false);
+        value_1 = (term.TargetStatus==="Choose..." ? true:false);
+        value_2 = (term.ThetaAngle==='' ? true:false);
+        value_3 = (term.PhiAngle==='' ? true:false);
+        value_4 = (term.RhoAngle==='' ? true:false);
+        value_5 = (term.TargetinfoExperimentDate==='' ? true:false);
+        value_6 = (term.TargetinfoExperimentTime==='' ? true:false);
+        value_7 = (term.TargetValidity===false ? true:false);
+        value_8 = (term.TargetXPosition==='' ? true:false);
+        value_9 = (term.TargetYPosition==='' ? true:false);
+        value_10 = (term.TargetZPosition==='' ? true:false);
+        value_11 = (term.InplaneAccuracy==='' ? true:false);
+        value_12 = (term.OutOfPlaneAccuracy==='' ? true:false);
+        value_13 = (term.TargetpositioninfoExperimentDate==='' ? true:false);
+        value_14 = (term.TargetpositioninfoExperimentTime==='' ? true:false);
+        value_15 = (term.InstrumentUsed==="Choose..." ? true:false);
+        value_16 = (term.XFELDataExperimentDate==='' ? true:false);
+        value_17 = (term.XFELDataExperimentTime==='' ? true:false);
+        result = (value_0 | value_1 | value_2 | value_3 | value_4 | value_5 | value_6 | value_7 | value_8 | 
+            value_9 | value_10 | value_11 | value_12 | value_13 | value_14 | value_15 | value_16 | value_17);
+    }
+
+    return result;
+}
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -80,88 +192,155 @@ app.post("/submitSubframeInfoManually", function(req, res){
     subframe_itemNamesData.SubframeExperimentDate = req.body.subframe_experiment_date;
     subframe_itemNamesData.SubframeExperimentTime = req.body.subframe_experiment_time;
     subframe_itemNamesData.SubframeEventTypeName = req.body.subframe_event_type_name;
-
-    console.log(subframe_itemNamesData);
-
-    //if (fiducialCounter < 4) {
-    //    fiducialPoints.push(fiducialList[fiducialCounter]);
-    //    fiducialCounter += 1;
-    //}
-
-    //res.render("manually_fiducial", {listFiducials:fiducialPoints});
-    res.render("manually_fiducial");
+    
+    if (checkEmpty(subframe_itemNamesData, "subframe")) {
+        let query = "SELECT COUNT(*) as count FROM Subframe; SELECT MAX(SerialNumber)+1 as max_sr FROM Subframe;";
+        connection.query(query, function(err, results){
+            if(err) 
+                throw err;
+            else
+                var count = results[0][0].count;
+            var max_sr = results[1][0].max_sr;        
+            res.render("home_manually_subframe", {data: count, serialnumber: max_sr});
+        });
+    } else {
+        res.render("manually_fiducial");
+    }    
 });
 
 app.post("/submitFiducialsInfoManually", function(req, res){
-    fiducials_itemNamesData.Fid1Validity = req.body.fid1_validity;
+    fiducials_itemNamesData.Fid1Validity = (req.body.fid1_validity ? true:false);
     fiducials_itemNamesData.Fid1PositionName = req.body.fid1_position_name;
     fiducials_itemNamesData.Fid1XPosition = req.body.fid1_x_position;
     fiducials_itemNamesData.Fid1YPosition = req.body.fid1_y_position;
     fiducials_itemNamesData.Fid1ZPosition = req.body.fid1_z_position;
 
-    fiducials_itemNamesData.Fid2Validity = req.body.fid2_validity;
+    fiducials_itemNamesData.Fid2Validity = (req.body.fid2_validity ? true:false);
     fiducials_itemNamesData.Fid2PositionName = req.body.fid2_position_name;
     fiducials_itemNamesData.Fid2XPosition = req.body.fid2_x_position;
     fiducials_itemNamesData.Fid2YPosition = req.body.fid2_y_position;
     fiducials_itemNamesData.Fid2ZPosition = req.body.fid2_z_position;
 
-    fiducials_itemNamesData.Fid3Validity = req.body.fid3_validity;
+    fiducials_itemNamesData.Fid3Validity = (req.body.fid3_validity ? true:false);
     fiducials_itemNamesData.Fid3PositionName = req.body.fid3_position_name;
     fiducials_itemNamesData.Fid3XPosition = req.body.fid3_x_position;
     fiducials_itemNamesData.Fid3YPosition = req.body.fid3_y_position;
     fiducials_itemNamesData.Fid3ZPosition = req.body.fid3_z_position;
 
-    fiducials_itemNamesData.Fid4Validity = req.body.fid4_validity;
+    fiducials_itemNamesData.Fid4Validity = (req.body.fid4_validity ? true:false);
     fiducials_itemNamesData.Fid4PositionName = req.body.fid4_position_name;
     fiducials_itemNamesData.Fid4XPosition = req.body.fid4_x_position;
     fiducials_itemNamesData.Fid4YPosition = req.body.fid4_y_position;
     fiducials_itemNamesData.Fid4ZPosition = req.body.fid4_z_position;
 
-    console.log(fiducials_itemNamesData);
-
-    res.render("manually_overview_image");
+    if (checkEmpty(fiducials_itemNamesData, "fiducial")) {
+        res.render("manually_fiducial");
+    } else {
+        res.render("manually_overview_image");
+    }
 });
 
 app.post("/submitOverviewImageManually", function(req, res){
-    overviewimage_itemNamesData.OverviewImageValidity = req.body.overview_image_validity;
+    overviewimage_itemNamesData.OverviewImageValidity = (req.body.overview_image_validity ? true:false);
     overviewimage_itemNamesData.MicroscopeData = req.body.microscope_data;
     overviewimage_itemNamesData.DetectionMethod = req.body.detection_method;
     overviewimage_itemNamesData.OverviewImageLoad = req.body.overview_image_load;
     overviewimage_itemNamesData.OverviewImageExperimentDate = req.body.overviewimage_experiment_date;
     overviewimage_itemNamesData.OverviewImageExperimentTime = req.body.overviewimage_experiment_time;
     
-    console.log(overviewimage_itemNamesData);
-
-    res.render("manually_roi_image");
+    if (checkEmpty(overviewimage_itemNamesData, "overviewimage")) {
+        res.render("manually_overview_image");
+    } else {
+        res.render("manually_roi_image");
+    }
 });
 
 app.post("/submitROIImageManually", function(req, res){
     
-    roi_itemNamesData.ROIImageValidity = req.body.roi_image_validity;
+    roi_itemNamesData.ROIImageValidity = (req.body.roi_image_validity ? true:false);
     roi_itemNamesData.MicroscopeData = req.body.microscope_data;
     roi_itemNamesData.DetectionMethod = req.body.detection_method;
     roi_itemNamesData.ROIOverviewImageLoad = req.body.roi_overviewimage_load;
     roi_itemNamesData.ROIExperimentDate = req.body.roi_experiment_date;
     roi_itemNamesData.ROIExperimentTime = req.body.roi_experiment_time;
-
-    console.log(roi_itemNamesData);
-
-    res.render("manually_sample_image");
+    
+    if (checkEmpty(roi_itemNamesData, "roi")) {
+        res.render("manually_roi_image");
+    } else {
+        res.render("manually_sample_image");
+    }
 });
 
 app.post("/submitSampleImageManually", function(req, res){
-    res.render("manually_target_data");
+    sample_itemNamesData.SampleImageValidity = (req.body.sample_image_validity ? true:false);
+    sample_itemNamesData.PixelSizeX = req.body.pixel_size_x;
+    sample_itemNamesData.PixelSizeY = req.body.pixel_size_y;
+    sample_itemNamesData.BBXPosition = req.body.BB_x_position;
+    sample_itemNamesData.BBYPosition = req.body.BB_y_position;
+    sample_itemNamesData.BBWidth = req.body.BB_width;
+    sample_itemNamesData.BBHeight = req.body.BB_height;
+    sample_itemNamesData.MaskImageLoad = req.body.mask_image_load;
+    sample_itemNamesData.SampleimageExperimentDate = req.body.sampleimage_experiment_date;
+    sample_itemNamesData.SampleimageExperimentTime = req.body.sampleimage_experiment_time;
+    sample_itemNamesData.SampleComments = req.body.sample_comments;
+    sample_itemNamesData.SampleInfoValidity = (req.body.sample_info_validity ? true:false);
+    sample_itemNamesData.SampleMicroscopeData = req.body.sample_microscope_data;
+    sample_itemNamesData.SampleDetectionMethod = req.body.sample_detection_method;
+    sample_itemNamesData.ROIImageLoad = req.body.roi_image_load;
+    sample_itemNamesData.ImageType = req.body.image_type;
+    sample_itemNamesData.ImageWidth = req.body.image_width;
+    sample_itemNamesData.ImageHeight = req.body.image_height;
+    sample_itemNamesData.ImageSize = req.body.image_size;
+    sample_itemNamesData.ImageComments = req.body.image_comments;
+    sample_itemNamesData.ImageLoad = req.body.image_load;
+    sample_itemNamesData.SampleinfoExperimentDate = req.body.sampleinfo_experiment_date;
+    sample_itemNamesData.SampleinfoExperimentTime = req.body.sampleinfo_experiment_time;
+
+    console.log(sample_itemNamesData);
+
+    if (checkEmpty(sample_itemNamesData, "sampleimage")) {
+        res.render("manually_sample_image");
+    } else {
+        res.render("manually_target_data");
+    }
 });
 
 app.post("/submitAllData", function(req, res){
-    let query = "SELECT COUNT(*) as count FROM Subframe";
-    connection.query(query, function (err, results) {
-        if (err)
-            throw err;
-        else
-            var count = results[0].count;
-        res.render("home", { data: count });
-    });
+    target_itemNamesData.TargetImageValidity = (req.body.target_image_validity ? true:false);
+    target_itemNamesData.TargetStatus = req.body.target_status;
+    target_itemNamesData.ThetaAngle = req.body.theta_angle;
+    target_itemNamesData.PhiAngle = req.body.phi_angle;
+    target_itemNamesData.RhoAngle = req.body.rho_angle;
+    target_itemNamesData.TargetinfoExperimentDate = req.body.targetinfo_experiment_date;
+    target_itemNamesData.TargetinfoExperimentTime = req.body.targetinfo_experiment_time;
+    target_itemNamesData.TargetComments = req.body.target_comments;
+    target_itemNamesData.TargetValidity = (req.body.target_validity ? true:false);
+    target_itemNamesData.TargetXPosition = req.body.target_x_position;
+    target_itemNamesData.TargetYPosition = req.body.target_y_position;
+    target_itemNamesData.TargetZPosition = req.body.target_z_position;
+    target_itemNamesData.InplaneAccuracy = req.body.inplane_accuracy;
+    target_itemNamesData.OutOfPlaneAccuracy = req.body.out_of_plane_accuracy;
+    target_itemNamesData.TargetpositioninfoExperimentDate = req.body.targetpositioninfo_experiment_date;
+    target_itemNamesData.TargetpositioninfoExperimentTime = req.body.targetpositioninfo_experiment_time;
+    target_itemNamesData.InstrumentUsed = req.body.instrument_used;
+    target_itemNamesData.XFELDataExperimentDate = req.body.xfeldata_experiment_date;
+    target_itemNamesData.XFELDataExperimentTime = req.body.xfeldata_experiment_time;
+    target_itemNamesData.TargetInstrumentComments = req.body.target_instrument_comments;
+    
+    console.log(target_itemNamesData);
+
+    if (checkEmpty(target_itemNamesData, "targetdata")) {
+        res.render("manually_target_data");
+    } else {
+        let query = "SELECT COUNT(*) as count FROM Subframe";
+        connection.query(query, function (err, results) {
+            if (err)
+                throw err;
+            else
+                var count = results[0].count;
+            res.render("home", { data: count });
+        });
+    }
 });
 
 
